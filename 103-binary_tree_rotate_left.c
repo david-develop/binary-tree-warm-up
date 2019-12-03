148e69c82979f5ee1499a1bc6c39aee37f6a8a2c
luischaparoc <lfchaparro@outlook.com>
2019-12-03 23:21:14 +0000

    Adding task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -11,20 +11,14 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	binary_tree_t *new_root;
 
 	new_root = tree->right;
-	new_root->left = tree;
 
-	new_root->parent = tree->parent;
-
-	if (tree->right->left)
-	{
-		tree->right->left->parent = new_root->left;
-		tree->right = tree->right->left;
-	}
+	if (new_root->left)
+		tree->right = new_root->left;
 	else
-	{
 		tree->right = NULL;
-	}
 
+	new_root->left = tree;
+	new_root->parent = tree->parent;
 	tree->parent = new_root;
 
 	return (new_root);
